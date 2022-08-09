26d79bc8cbe18713eba8da911172dce670a82fd6
erco <e.r.c.o@icloud.com>
2022-08-09 19:15:11 +0200

    RWD updated

--- menu_list.js
+++ menu_list.js
@@ -1,5 +1,22 @@
-Const metal_list = [
-  "Asterisk",
-  "2",
-  "3",
-];
+var metal_list = ["Asterisk", "Hopper", "Waterfall", "Factory"];
+var $spans = $("span.ml");
+
+$spans.eq(0).text(metal_list[0]);
+$spans.eq(1).text(metal_list[1]);
+$spans.eq(2).text(metal_list[2]);
+
+
+
+var earth_list = ["Mineralloop", "Neo Stone Age", "earth-3", "earth-4"];
+var $spans = $("span.el");
+
+$spans.eq(0).text(earth_list[0]);
+$spans.eq(1).text(earth_list[1]);
+$spans.eq(2).text(earth_list[2]);
+
+var wood_list = ["Treekind", "wood-2", "wood-3", "wood-4"];
+var $spans = $("span.wl");
+
+$spans.eq(0).text(wood_list[0]);
+$spans.eq(1).text(wood_list[1]);
+$spans.eq(2).text(wood_list[2]);
