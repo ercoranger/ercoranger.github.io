dcc55809805bfd4edc005a18b6cf8a1415f55e9c
erco <e.r.c.o@icloud.com>
2024-04-07 18:56:40 +0200

    update hereHere partly now hereHere can be used universally

--- js/hereHere.js
+++ js/hereHere.js
@@ -1,24 +1,37 @@
 $(function() {
-  $(window).scroll(function() {
-    var scrollTop = $(this).scrollTop();
-    var windowHeight = $(this).height();
-    var buffer = windowHeight * 0.05;
+    $(window).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        var windowHeight = $(this).height(); // Total height of the viewport
+        var buffer = windowHeight * 0.05; // 5% buffer of the total height
 
-    var distanceToSection1 = $('#sec-1').offset().top - buffer;
-    var distanceToSection2 = $('#sec-2').offset().top - buffer;
-    var distanceToSection3 = $('#sec-3').offset().top - buffer;
+        var sections = [];
+        var contentMenus = [];
 
-    if (scrollTop <= 300) {
-      $('.here-here').removeClass('here-here');
-    } else if (scrollTop >= distanceToSection1 && scrollTop < distanceToSection2) {
-      $('.here-here').removeClass('here-here');
-      $('#content-menu-1').addClass('here-here');
-    } else if (scrollTop >= distanceToSection2 && scrollTop < distanceToSection3) {
-      $('.here-here').removeClass('here-here');
-      $('#content-menu-2').addClass('here-here');
-    } else if (scrollTop >= distanceToSection3) {
-      $('.here-here').removeClass('here-here');
-      $('#content-menu-3').addClass('here-here');
-    }
-  });
+        // Populate sections and contentMenus arrays based on existing sections
+        for (var i = 1; ; i++) {
+            var sectionId = '#sec-' + i;
+            var contentMenuId = '#content-menu-' + i;
+
+            var section = $(sectionId);
+            if (!section.length) break; // Break the loop if section doesn't exist
+
+            var sectionOffset = section.offset();
+            if (sectionOffset && sectionOffset.top !== 0) {
+                sections.push(sectionOffset.top - buffer);
+                contentMenus.push(contentMenuId);
+            }
+        }
+
+        $('.here-here').removeClass('here-here');
+
+        // Adjust the loop condition based on the number of sections
+        for (var j = sections.length - 1; j >= 0; j--) {
+            if (scrollTop >= sections[j]) {
+                $(contentMenus[j]).addClass('here-here');
+                break;
+            }
+        }
+    });
 });
+
+//by chatGPT, 2024
