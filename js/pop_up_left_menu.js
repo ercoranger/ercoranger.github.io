f2a73d53ec352a6f1d7116242719824bf9752901
erco <e.r.c.o@icloud.com>
2024-06-06 20:12:29 +0200

    1. add material info 2. add/fix exhibition section 3. fix minor problem of layout 4. fix few problems

--- js/pop_up_left_menu.js
+++ js/pop_up_left_menu.js
@@ -3,13 +3,13 @@
     $(window).scroll(function() {
       var scrollVal = $(this).scrollTop();
 
-      if (scrollVal >= 200) {
+      if (scrollVal >= 300) {
         $('#content-menu').addClass('content-menu');
         $('#content-menu').removeClass('content-menu-off');
 
       }
 
-      if (scrollVal <= 200) {
+      if (scrollVal <= 300) {
         $('#content-menu').removeClass('content-menu');
         $('#content-menu').addClass('content-menu-off');
 
