7fc45e4e806ee8f4821ba44d8e5c5183e67b98d6
erco <e.r.c.o@icloud.com>
2024-06-08 13:30:55 +0200

    change remove li into footer

--- js/removeListByTitle.js
+++ js/removeListByTitle.js
@@ -1,14 +1,14 @@
-$(document).ready(() => {
-  $("#ee-header").load("components/header.html", () => {
-    const pageTitle = document.title;
-    const anchors = document.querySelectorAll('.ee-right-menu ul a');
+const removeCurrentPageLink = () => {
+  const pageTitle = document.title;
+  const anchors = document.querySelectorAll('.ee-right-menu ul a');
 
-    for (let i = 0; i < anchors.length; i++) {
-      const anchor = anchors[i];
-      if (anchor.textContent.trim() === pageTitle) {
-        anchor.remove();
-        break; // Exit the loop once the item is found and removed
-      }
+  for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
+    if (anchor.textContent.trim() === pageTitle) {
+      anchor.remove();
+      break; // Exit the loop once the item is found and removed
     }
-  });
-});
+  }
+};
+
+$("#ee-header").load("components/header.html", removeCurrentPageLink);
