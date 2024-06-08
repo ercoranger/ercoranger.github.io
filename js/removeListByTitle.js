6655fcc0674455247d413ea7d55c04b6c6904402
erco <e.r.c.o@icloud.com>
2024-06-08 13:50:20 +0200

    move "remove li" to header

--- js/removeListByTitle.js
+++ js/removeListByTitle.js
@@ -1,14 +1,14 @@
 const removeCurrentPageLink = () => {
-  const pageTitle = document.title;
-  const anchors = document.querySelectorAll('.ee-right-menu ul a');
+   const pageTitle = document.title;
+   const anchors = document.querySelectorAll('.ee-right-menu ul a');
 
-  for (let i = 0; i < anchors.length; i++) {
-    const anchor = anchors[i];
-    if (anchor.textContent.trim() === pageTitle) {
-      anchor.remove();
-      break; // Exit the loop once the item is found and removed
-    }
-  }
-};
+   for (let i = 0; i < anchors.length; i++) {
+     const anchor = anchors[i];
+     if (anchor.textContent.trim() === pageTitle) {
+       anchor.remove();
+       break; // Exit the loop once the item is found and removed
+     }
+   }
+ };
 
-$("#ee-header").load("components/header.html", removeCurrentPageLink);
+ $("#ee-header").load("components/header.html", removeCurrentPageLink);
