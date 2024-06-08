e6d58d8ecba5e9f89e00160bbfa33ac8831e8b83
erco <e.r.c.o@icloud.com>
2024-06-08 15:19:41 +0200

    speed up "remove li"

--- js/removeListByTitle.js
+++ js/removeListByTitle.js
@@ -1,14 +1,15 @@
-const removeCurrentPageLink = () => {
-   const pageTitle = document.title;
-   const anchors = document.querySelectorAll('.ee-right-menu ul a');
 
-   for (let i = 0; i < anchors.length; i++) {
-     const anchor = anchors[i];
-     if (anchor.textContent.trim() === pageTitle) {
-       anchor.remove();
-       break; // Exit the loop once the item is found and removed
-     }
-   }
- };
+    const removeCurrentPageLink = () => {
+      const pageTitle = document.title;
+      const anchors = document.querySelectorAll('.ee-right-menu ul a');
 
- $("#ee-header").load("components/header.html", removeCurrentPageLink);
+      for (let i = 0; i < anchors.length; i++) {
+        const anchor = anchors[i];
+        if (anchor.textContent.trim() === pageTitle) {
+          anchor.remove();
+          break; // Exit the loop once the item is found and removed
+        }
+      }
+    };
+
+    removeCurrentPageLink();
