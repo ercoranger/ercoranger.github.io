564809602eae787b05e0f72c4235c1bf201a1b9b
erco <e.r.c.o@icloud.com>
2024-06-09 18:18:06 +0200

    merge scroll-related into one: here-here and fix few bugs

--- js/backToTop.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-//動畫方式回到上層
-$("#gotop").click(function(){
-    jQuery("html,body").animate({
-    scrollTop:0
-  },2000);
-});
-
-});
--- js/hereHere.js
+++ js/hereHere.js
@@ -1,37 +1,47 @@
 $(function() {
+    var buffer = $(window).height() * 0.3; // buffer of the total height
+    var sections = [];
+    var contentMenus = [];
+
+    // Populate sections and contentMenus arrays based on existing sections
+    for (var i = 1; ; i++) {
+        var section = $('#sec-' + i);
+        if (!section.length) break; // Break the loop if section doesn't exist
+
+        sections.push(section.offset().top - buffer);
+        contentMenus.push('#content-menu-' + i + ' li');
+    }
+
     $(window).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-        var windowHeight = $(this).height(); // Total height of the viewport
-        var buffer = windowHeight * 0.3; // buffer of the total height
+        var scrollVal = $(this).scrollTop();
 
-        var sections = [];
-        var contentMenus = [];
-
-        // Populate sections and contentMenus arrays based on existing sections
-        for (var i = 1; ; i++) {
-            var sectionId = '#sec-' + i;
-            var contentMenuId = '#content-menu-' + i;
-
-            var section = $(sectionId);
-            if (!section.length) break; // Break the loop if section doesn't exist
-
-            var sectionOffset = section.offset();
-            if (sectionOffset && sectionOffset.top !== 0) {
-                sections.push(sectionOffset.top - buffer);
-                contentMenus.push(contentMenuId);
-            }
-        }
-
+        // Highlighting menu items based on scroll position
         $('.here-here').removeClass('here-here');
-
-        // Adjust the loop condition based on the number of sections
         for (var j = sections.length - 1; j >= 0; j--) {
-            if (scrollTop >= sections[j]) {
+            if (scrollVal >= sections[j]) {
                 $(contentMenus[j]).addClass('here-here');
                 break;
             }
         }
+
+        if (scrollVal >= 240) {
+           $('#content-menu').addClass('content-menu');
+           $('#content-menu').removeClass('content-menu-off');
+       } else {
+           $('#content-menu').removeClass('content-menu');
+           $('#content-menu').addClass('content-menu-off');
+       }
+    });
+
+    // Smooth scrolling to sections
+    $('[id^=content-menu-] li').click(function() {
+        var sectionIndex = $(this).parent().attr('id').split('-')[2];
+        var sectionOffset = $('#sec-' + sectionIndex).offset().top;
+        $('html, body').animate({ scrollTop: sectionOffset - 50 }, 1000); // 1000 ms for smooth scrolling animation
+    });
+
+    // Smooth scrolling to the top
+    $("#gotop").click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 2000); // 2000 ms for smooth scrolling animation to the top
     });
 });
-
-//by chatGPT, 2024
