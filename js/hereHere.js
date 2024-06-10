9033c98e9544244b33936586e843a07b43df4728
erco <e.r.c.o@icloud.com>
2024-06-10 16:53:42 +0200

    herehere not working still

--- js/hereHere.js
+++ js/hereHere.js
@@ -1,28 +1,55 @@
 $(function() {
-    var buffer = $(window).height() * 0.3; // buffer of the total height
+    var buffer = $(window).height() * 0.3; // Buffer of the total height
     var sections = [];
     var contentMenus = [];
 
-    // Populate sections and contentMenus arrays based on existing sections
-    for (var i = 1; ; i++) {
-        var section = $('#sec-' + i);
-        if (!section.length) break; // Break the loop if section doesn't exist
+    // Function to update sections and contentMenus arrays
+    function updateSections() {
+        sections = [];
+        contentMenus = [];
+        for (var i = 1; ; i++) {
+            var section = $('#sec-' + i);
+            if (!section.length) break; // Break the loop if section doesn't exist
 
-        var sectionOffsetTop = section.offset().top;
-        sections.push(sectionOffsetTop);
-        contentMenus.push('#content-menu-' + i);
+            var sectionOffsetTop = section.offset().top;
+            sections.push(sectionOffsetTop);
+            contentMenus.push('#content-menu-' + i);
 
-        // Log each section's scrollTop value
-        console.log('Section ' + i + ' scrollTop: ' + sectionOffsetTop);
+            // Log each section's offsetTop value
+            console.log('Section ' + i + ' offsetTop: ' + sectionOffsetTop);
+        }
     }
+
+    // Debounce function to limit the rate of function execution
+    function debounce(func, wait) {
+        var timeout;
+        return function() {
+            clearTimeout(timeout);
+            timeout = setTimeout(func, wait);
+        };
+    }
+
+    // Initial update of sections and contentMenus after page fully loads
+    $(window).on('load', function() {
+        updateSections();
+    });
+
+    // Update sections and contentMenus on window resize with debounce
+    $(window).resize(debounce(function() {
+        updateSections();
+    }, 100));
 
     $(window).scroll(function() {
         var scrollVal = $(this).scrollTop();
+        console.log('scrollTop: ' + scrollVal); // Log current scroll position
 
         // Highlighting menu items based on scroll position
         $('.here-here').removeClass('here-here');
         for (var j = sections.length - 1; j >= 0; j--) {
-            if (scrollVal >= sections[j] - buffer) {
+            var screenTop = sections[j] - buffer;
+            console.log('Section ' + (j + 1) + ' screenTop: ' + screenTop); // Log screenTop for each section
+
+            if (scrollVal >= screenTop) {
                 $(contentMenus[j] + ' li').addClass('here-here');
                 break;
             }
